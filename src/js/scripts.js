8e40f99cf925a5ce5be1500e7faad9e01a9e7cf8
Scott Fowles <scott.fowles@dreamhost.com>
2017-01-13 15:28:06 -0800

    Popovers first pass.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,3 +10,25 @@ $link.click(function(){
 	$(".tabs__nav-item[data-tab='" + x + "']").addClass('is-active').siblings().removeClass('is-active');
 	$(".tabs").find("[data-tab='" + x + "']").show();
 })
+
+// Popovers
+var $button = $('.btn,.popover');
+
+$button.on('click', function (e) {
+	var $popover = $(this).closest('.l-pos-relative:not(.popover--hover)').find('.popover');
+
+	if ($(this).attr('data-popover')) {
+		if ($popover.hasClass('is-open')) {
+			$('.popover').removeClass('is-open');
+		} else {
+			$('.popover').removeClass('is-open');
+			$popover.addClass('is-open');
+		}
+	}
+	e.stopPropagation();
+});
+
+$(document).on('click', function (e) {
+	if (!$(this).attr('data-popover'))
+		$('.popover').removeClass('is-open');
+});
