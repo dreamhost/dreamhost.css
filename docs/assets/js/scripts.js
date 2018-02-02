06caada18021db04f4a378c14442d96601536ea3
Chris Leckie <chris.leckie@dreamhost.com>
2018-02-02 15:09:46 -0800

    remove docs as they're now in separate repo

--- docs/assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// SideNav mobile click handler
-$( document ).ready(function() {
-    $('.js-SideNav-mobile').on('click', function() {
-        $('.SideNav').toggleClass('SideNav--show');
-        $('.SideNav-mobile').toggleClass('SideNav-mobile--white');
-    });
-});
