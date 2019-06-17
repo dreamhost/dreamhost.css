6201bdec5165f491a2d268217a0ccdd43e175cfd
BillyH80 <BillyH80@users.noreply.github.com>
2019-06-17 15:10:29 -0700

    0.4.0 (#261)

--- dist/js/dreamhost.js
+++ dist/js/dreamhost.js
@@ -802,6 +802,7 @@
 		Accordion.load();
 		Tabs.load();
 		Quickcopy.load();
+		Bars.load();
 	}
 
 	var Accordion = {
@@ -870,6 +871,49 @@
 		}
 	}
 
+	var Bars = {
+		load: function() {
+			if(!$('.Bar').length) return;
+			Bars.load_listeners();
+		},
+		reset_drawers: function($parent, class_to_toggle) {
+			var toggle_classes = ['is-open', 'delete-mode', 'edit-mode'];
+			var classes_to_untoggle = toggle_classes.filter(function(class_to_untoggle) {
+				return class_to_untoggle != class_to_toggle;
+			});
+
+			classes_to_untoggle = classes_to_untoggle.join(' ');
+			$parent.removeClass(classes_to_untoggle);
+		},
+		toggle_drawer: function($el, class_to_toggle) {
+			var $parent = $el.closest('.js-bar');
+
+			Bars.reset_drawers($parent, class_to_toggle);
+			$parent.toggleClass(class_to_toggle);
+		},
+		toggle_drawer_on_click: function() {
+			$('.js-drawer-on-click').on('click', function() {
+				Bars.toggle_drawer($(this), 'is-open');
+			});
+		},
+		toggle_delete_mode_on_click: function() {
+			$('.js-delete-mode-on-click').on('click', function() {
+				Bars.toggle_drawer($(this), 'delete-mode');
+			});
+		},
+		toggle_edit_mode_on_click: function() {
+			$('.js-edit-mode-on-click').on('click', function() {
+				Bars.toggle_drawer($(this), 'edit-mode');
+			});
+		},
+		load_listeners: function() {
+			if(!$('.js-bar').length) return;
+			Bars.toggle_drawer_on_click();
+			Bars.toggle_delete_mode_on_click();
+			Bars.toggle_edit_mode_on_click();
+		}
+	}
+
 	window.onload = dh_css.init();
 
 })(window.dh_css = window.dh_css || {}, jQuery);
@@ -930,55 +974,6 @@ $('.Toaster').on('click', function () {
 		}, 300);
 	}
 });
-
-(function(bars, $) {
-	var isClick;
-
-	bars.init = function() {
-		events.load_listeners();
-	}
-
-	var events = {
-		reset_drawers: function($parent, class_to_toggle) {
-			var toggle_classes = ['is-open', 'delete-mode', 'edit-mode'];
-			var classes_to_untoggle = toggle_classes.filter(function(class_to_untoggle) {
-				return class_to_untoggle != class_to_toggle;
-			});
-
-			classes_to_untoggle = classes_to_untoggle.join(' ');
-			$parent.removeClass(classes_to_untoggle);
-		},
-		toggle_drawer: function($el, class_to_toggle) {
-			var $parent = $el.closest('.js-bar');
-
-			events.reset_drawers($parent, class_to_toggle);
-			$parent.toggleClass(class_to_toggle);
-		},
-		toggle_drawer_on_click: function() {
-			$('.js-drawer-on-click').on('click', function() {
-				events.toggle_drawer($(this), 'is-open');
-			});
-		},
-		toggle_delete_mode_on_click: function() {
-			$('.js-delete-mode-on-click').on('click', function() {
-				events.toggle_drawer($(this), 'delete-mode');
-			});
-		},
-		toggle_edit_mode_on_click: function() {
-			$('.js-edit-mode-on-click').on('click', function() {
-				events.toggle_drawer($(this), 'edit-mode');
-			});
-		},
-		load_listeners: function() {
-			if(!$('.js-bar').length) return;
-			events.toggle_drawer_on_click();
-			events.toggle_delete_mode_on_click();
-			events.toggle_edit_mode_on_click();
-		}
-	}
-
-	window.onload = bars.init();
-})(window.bars = window.bars || {}, jQuery);
 
 /*
      _ _      _       _
